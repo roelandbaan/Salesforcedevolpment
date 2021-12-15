3e98e9a836a70ff497089b075e01e1daf117012e
roelandbaan <roelandbaan1982@gmail.com>
2021-12-15 21:57:53 +0100

    day 2

--- JS/JS Developer/Variables 1 JS.js
+++ JS/JS Developer/Variables 1 JS.js
@@ -92,3 +92,83 @@ const record = [
      if(typeof role == 'undefined') {
          role = 'Major';
      }
+
+     console.log(record);
+
+     console.log(Array.from('123'));
+     console.log(Array.of("een", "twee", "drie"));
+     console.log(Array.isArray(["I am", "an", "Array"]));
+
+     let quizScores = new Map();
+     quizScores.set('quiz1', [12, 14, 15]);
+     quizScores.set('quiz2', [34, 65, 34, 23]);
+     quizScores.set('quiz3', [3,5]);
+     console.log(quizScores);
+
+     const str2 = new String('This is a string constructor'); 
+
+     function greeting(greet, name)
+     {
+         let punct = '.';
+         if(greet.toLowerCase().includes('good morning')) 
+         {
+             punct = '!';
+         }
+         return greet.concat(' ', name, punct);
+     }
+
+     console.log(greeting('Welcome and Good Morning', 'James'));
+
+     const str3 = 'Welcome';
+     const str4 = 'Good Morning';
+     const greeting1 = str3 + ' and ' + str4;
+     console.log(greeting1);
+
+     function message1 (issue, numSamples) 
+     {
+         let intro = 'Pause before proceeding:';
+         let timePerSample = 30;
+         return `${intro} ${issue} Wait ${timePerSample * 5} seconds.`;
+     }
+
+console.log(message1('The samples are not ready.', 5));
+
+const num1 = 3.14;
+
+const num2 = new Number(44.34);
+
+function stringToNum(str) 
+{
+    if(Number.isNaN(str)) 
+    {
+        throw 'String cannot be converted';
+    }
+    if(Number.isInteger(str)) return Number.parseInt(str); return Number.parseFloat(str);
+}
+
+console.log(stringToNum('5.65.abcdse'));
+
+
+
+const start = new Date();
+
+console.log(start - new Date(Date.UTC(2000, 0, 0, 0, 0, 0)));
+//check how long script takes to run with  this code
+console.log(Date.now() - Date.parse(start));
+
+function dateFormat(date) 
+{
+    return `${date.getDate()}-${date.getMonth() +1}-${date.getFullYear()}`;
+}
+
+console.log(dateFormat(start));
+
+let d = new Date();
+
+console.log(d.toString());
+
+console.log(d.toDateString());
+
+console.log(d.toUTCString());
+
+console.log(d.toISOString());
