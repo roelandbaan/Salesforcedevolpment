07b3575483b9761a8b171a47e00a1c9736a23b3c
roelandbaan <roelandbaan1982@gmail.com>
2022-01-12 16:33:32 +0100

    functions

--- JS/JS Developer/Objects Functions and Classes/Objects FUnctions and Classes 1.js
+++ JS/JS Developer/Objects Functions and Classes/Objects FUnctions and Classes 1.js
@@ -56,7 +56,7 @@ console.log(newNums);
 
 
 const createMultiplyFunction = function(num) {
-    console.log(num);
+    //console.log(num);
     return num1 => num1  * num;
 }
 
@@ -77,12 +77,94 @@ date = new Date();
 intro = "Today's date is";
 console.log('Program is running!');
 
+sumDeclaration(5, 10);
+//sumExpression(50, 100);
+
+const sumExpression = function(num1, num2) {
+    console.log(num1 + num2);
+}
+
+sumExpression(50, 100);
+
+function sumDeclaration(num1, num2) {
+    console.log(num1 + num2);
+}
+
 function createScoreFunction() {
     scores = [];
     return function(score) {
         scores.push(score);
-        console.log(`Total scores: ${scores.lenght}`);
+        console.log(`Total scores: ${scores.length}`);
     }
 };
 
 const addScore = createScoreFunction();
+
+
+function createScoreFunction1() {
+    scores = [];
+    return function(score) { 
+        scores.push(score);
+        console.log(`Total scores: ${scores.length}`);
+        }
+       };
+       
+const addScore1 = createScoreFunction1();
+
+let objGreet = {
+    morningGreet(punct) {
+        console.log(`Good morning ${this.firstName}${punct}`);
+    },
+    eveningGreet(punct) {
+        console.log(`Good evening ${this.firstName}${punct}`);
+    }
+}
+
+let user1 = {
+    firstName: 'Mary'
+};
+let user2 = {
+    firstName: 'Sam'
+};
+
+//call parameters are passed in as comma seperated list
+objGreet.morningGreet.call(user1, '!');
+//apply parameters are passed in as an array
+objGreet.eveningGreet.apply(user2, ['?']);
+// bind arguments returns a new function
+objGreet.eveningGreet.bind(user1, '.')();
+
+//generatorfuncion *
+function* fibonacciSeq() {
+    let start1 = 0, start2 = 1, cnt = 2;
+    const sequence = [start1, start2];
+    while(cnt <= 100) {
+        let newNum = start1 + start2;
+        start1 = start2;
+        start2 = newNum;
+        cnt++;
+        sequence.push(newNum);
+        yield sequence;
+    }
+};
+
+const fib100 = fibonacciSeq();
+
+
+//iterator function returns an iterator object
+function progressiveFactoral(upperLimit) {
+    let factorial = 1, num = 1;
+    const factorialIterator = {//this is the iterator object
+        next() { //the next method is required for the iterator object
+            if(num <= upperLimit) {
+                num++;
+                factorial = factorial + num;
+                return {value: {number: num, factorial},done: false}
+            }
+            return {value:{number: num, factorial}, done: true};
+        }
+    };
+    return factorialIterator; // return the iterator object
+}
+
+let factorial10 = progressiveFactoral(10);
